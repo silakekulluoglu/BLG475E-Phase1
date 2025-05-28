0013aee7ad2149d29d198ea83dbfbb5271dcbb12
silakekulluoglu <kekulluoglu21@itu.edu.tr>
2025-05-29 02:48:57 +0300

    Added test results for mistral_instruct

--- regenerated_codes/mistral_instruct/moderate/human_eval_75.py
+++ regenerated_codes/mistral_instruct/moderate/human_eval_75.py
@@ -7,23 +7,16 @@ Student IDs: 150190007, 150200913, 150210084
 from math import sqrt
 
 def is_multiply_prime(a):
-    if a < 2 or (a % 2 == 0 and a != 2):
+    if a <= 1 or a > 96:
         return False
 
-    prime_multipliers = [2, 3]
-    multiple = 2
+    primes = [2, 3, 5]
+    factors = set()
+    for p in primes:
+        while a % p == 0:
+            factors.add(p)
+            a //= p
+    if len(factors) != 3 or a > 1:
+        return False
 
-    while len(prime_multipliers) < 3:
-        multiple += 1
-        if a % multiple != 0:
-            continue
-
-        is_prime = True
-        for potential_divisor in range(2, int(multiple**0.5)+1):
-            if multiple % potential_divisor == 0:
-                is_prime = False
-                break
-        if is_prime:
-            prime_multipliers.append(multiple)
-
-    return all([x <= a for x in prime_multipliers]) and len(prime_multipliers) == 3
+    return all([f in primes for f in factors])
